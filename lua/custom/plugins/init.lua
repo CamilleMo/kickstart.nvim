f134403e25f215d06d9e5c407207b1f410bdb39e
Camille <camille.moatti@gmail.com>
2024-04-23 12:41:50 +0200

    add neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -52,4 +52,16 @@ return {
       -- see below for full list of options 👇
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+      -- 'ibhagwan/fzf-lua', -- optional
+    },
+    config = true,
+  },
 }
