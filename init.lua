4789fe50af286ea5ec8fc79a3d99f661c362e160
Camille <camille.moatti@gmail.com>
2025-05-16 17:17:47 +0200

    fix

--- init.lua
+++ init.lua
@@ -163,16 +163,11 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-<<<<<<< HEAD
-vim.opt.scrolloff = 100
-
+vim.o.scrolloff = 100
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.opt.confirm = true
-=======
-vim.o.scrolloff = 10
->>>>>>> c92ea7c (Replace vim.opt with vim.o (#1495))
+vim.o.confirm = true
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
