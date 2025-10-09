b57031ddd833f5571c7c6a791b129d0df1ae7bae
Camille <camille.moatti@gmail.com>
2025-10-09 16:13:31 +0200

    remove mason, lsp 0.11 way

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,8 +1,14 @@
-local none_ls = require 'null-ls'
-local opts = {
-  -- sources = {
-  --   -- none_ls.builtins.diagnostics.mypy,
-  --   -- none_ls.builtins.diagnostics.ruff,
-  -- },
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+  {
+    'nvimtools/none-ls.nvim',
+    ft = { 'python' },
+    -- opts = function()
+    --   return require 'custom.plugins.none-ls'
+    -- end,
+  },
 }
-return opts
